c9b5dabe6e1a942e91677cfc4ae6bec9c92b34bc
kk0ya <87171894+kk0ya@users.noreply.github.com>
2021-07-25 18:33:19 +0900

    Create 행렬생성기.c

---
+++ 행렬생성기.c
@@ -0,0 +1,40 @@
+//행렬을 for문으로 생성
+//주대각 성분만 출력
+
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <stdio.h>
+
+int main() {
+	int matrix[8][8];
+	int i, k;
+	int val = 1;
+
+	for (i = 0; i < 8; i++) {
+		for (k = 0; k < 8; k++) {
+			matrix[i][k] = val;
+			val++;
+		}
+	}
+
+	//행렬 출력
+	for (i = 0; i < 8; i++) {
+		for (k = 0; k < 8; k++) {
+			printf("%3d",matrix[i][k]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	printf("주대각 성분\n");
+	for (i = 0; i < 8; i++) {
+		for (k = 0; k < 8; k++) {
+			if (i == k) {
+				printf("%3d",matrix[i][k]);
+			}
+		}
+	}
+	printf("\n");
+}
